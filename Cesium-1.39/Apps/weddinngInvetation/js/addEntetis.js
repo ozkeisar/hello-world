8cd60e5132a1c2c2b57b4cd8c7811611036bc4ea
strengthCaesar <caesar7670@live.com>
2017-12-22 12:30:45 +0200

    mobile good

--- Cesium-1.39/Apps/weddinngInvetation/js/addEntetis.js
+++ Cesium-1.39/Apps/weddinngInvetation/js/addEntetis.js
@@ -1,3 +1,4 @@
+var buttonStyle;
 function requestFullScreen() {
 
     var el = document.body;
@@ -120,6 +121,114 @@ if( isMobile.any() ) {
   text-align: center;
   font-weight: bold;
   font-size: 15px;
+  padding: 14px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button4 span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button4 span:after {
+  content: '\\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button4:hover span {
+  padding-right: 25px;
+}
+
+.button4:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+
+</style>
+`;
+}else{
+    buttonStyle = `
+<style>
+    .button {
+    
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+}
+
+.button-empty {
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+
+.button3 {
+    background-color: white; 
+    color: black; 
+    border: 2px solid #f44336;
+}
+
+.all-line{
+    width: 100%;
+}
+
+.button3:hover {
+    background-color: #f44336;
+    color: white;
+}
+
+.button2:hover {
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24),0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+.green{
+    background-color: #4CAF50; /* Green */
+}
+
+.blue{
+    background-color: #4CAF50; /* Green */
+}
+
+.center{
+    position: absolute;
+    right: 36%;
+}
+
+.button4 {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: BLACK;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
   padding: 14px;
   transition: all 0.5s;
   cursor: pointer;
